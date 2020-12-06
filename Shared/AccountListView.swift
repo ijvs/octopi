ea7f5c1bd73fa32d779067a93961688eda002b99
Jonathan Velazquez <ijonathanvs@gmail.com>
2020-12-06 17:47:04 -0600

    Removes template code.

---
+++ Shared/AccountListView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountListView.swift
+//  octopi
+//
+//  Created by Jony Money on 12/6/20.
+//
+
+import SwiftUI
+
+struct AccountListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountListView()
+    }
+}
