a1dc4731b54bbc7e09060504313f84d66f6bbbe7
Jonathan Velazquez <ijonathanvs@gmail.com>
2020-12-06 18:10:14 -0600

    Adds SwiftOTP algorithms

--- Shared/Models/Algorithm.swift
+++ Shared/Models/Algorithm.swift
@@ -6,9 +6,21 @@
 //
 
 import Foundation
+import SwiftOTP
 
 enum Algorithm: String, Codable {
     case sha1 = "SHA1"
     case sha256 = "SHA256"
     case sha512 = "SHA512"
+    
+    var totp: OTPAlgorithm {
+        switch self {
+        case .sha1:
+            return .sha1
+        case .sha256:
+            return .sha256
+        case .sha512:
+            return .sha512
+        }
+    }
 }
