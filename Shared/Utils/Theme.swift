7a6b4b7b6cf48bd667b444bf60585462e35eb466
Jonathan Velazquez <ijonathanvs@gmail.com>
2020-12-06 18:01:25 -0600

    Adds theme and colors.

---
+++ Shared/Utils/Theme.swift
@@ -0,0 +1,24 @@
+//
+//  Theme.swift
+//  octopi
+//
+//  Created by Jony Money on 12/6/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct Theme {
+    
+    struct Colors {
+        static let background: Color = Color("Background")
+        static let secondaryBackground: Color = Color("SecondaryBackground")
+        static let ternaryBackground: Color = Color("TernaryBackground")
+        static let buttonBackground: Color = Color("ButtonBackground")
+        static let label: Color = Color("Label")
+        static let secondaryLabel: Color = Color("SecondaryLabel")
+        static let ternaryLabel: Color = Color("TernaryLabel")
+        static let buttonLabel: Color = Color("ButtonLabel")
+        static let tint: Color = Color("Tint")
+    }
+}
